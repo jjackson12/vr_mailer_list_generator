39ae4a6056361ba8b5c77aabf86836f41aeddee6
jjackson12 <jacob_jackson@brown.edu>
2025-09-15 15:48:44 -0600

    removing dupes in config file

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-BIGQUERY_SERVICE_ACCOUNT_FILE = "vr-mail-generator-8e97a63564fe.json"
-BUCKETS_SERVICE_ACCOUNT_KEY = "vr-mail-generator-56bee8a8278b.json"
 BUCKET_NAME = "vr_mail_lists"
 REVIEWER_EMAIL = "jjjackson116@gmail.com"
 BUCKETS_SERVICE_ACCOUNT_KEY = "vr-mail-generator-56bee8a8278b.json"
